dece4bf9a83283cbfbaba190fff4788d0ec044ef
Ilhamm21 <pratamailham351@gmail.com>
2024-06-19 09:24:10 +0700

    MoveCharacter

---
+++ Assets/Resources/Script/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float HorizontalInput;
+    public float speed = 20f;
+    public float XRange;
+
+    public GameObject projectilePrefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < -XRange)
+        {
+            transform.position = new Vector3(-XRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > XRange)
+        {
+            transform.position = new Vector3(XRange, transform.position.y, transform.position.z);
+        }
+
+        HorizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * speed * HorizontalInput);
+
+        // if (Input.GetButtonDown(KeyCode.Space))
+        // {
+        //     Instantiate(projectilePrefabs, transform.position, projectilePrefabs.transform.rotation);
+        // }
+    }
+}
